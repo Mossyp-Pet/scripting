c32cee7a83d655d5a55e158d721a38c2fa9fa822
Mossyp-Pet <120493296+Mossyp-Pet@users.noreply.github.com>
2022-12-20 20:53:19 +0300

    Add files via upload

---
+++ NPCScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCScript : MonoBehaviour
+{
+    //
+    public int health = 5;
+    public int level = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(health+=level);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        float speed = 1.2f;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
